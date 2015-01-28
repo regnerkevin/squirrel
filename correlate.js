bcf98b3ac968eef0cb8750354d6d68249a9e9aae
Brian Capouch <brianc@saintjoe.edu>
2015-01-28 02:50:21 -0500

    Process simple event correlations

---
+++ correlate.js
@@ -0,0 +1,26 @@
+load('journal.js');
+function hasEvent(event, entry) {
+  return entry.events.indexOf(event) != -1;
+}
+
+function tableFor(event, journal) {
+  var table = [0, 0, 0, 0];
+  for (var i = 0; i < journal.length; i++) {
+    var entry = journal[i], index = 0;
+    if (hasEvent(event, entry)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2]));
+}
+
+print(tableFor("touched tree", JOURNAL));
+print(phi(tableFor("touched tree",JOURNAL)));
